056c3ac41b625f734c89ffc035cd81f38cff5500
Kalle <karlfock@gmail.com>
2014-03-18 22:49:40 +0100

    refactor upload ctrl

--- app/js/controllers/uploadCtrl.js
+++ app/js/controllers/uploadCtrl.js
@@ -2,9 +2,11 @@
 
 var Track = require("../lib/track");
 
+
 module.exports = function($scope, $upload, $location, $route, uploadedTracks, $q) {
 
-    var numUploads, uploaded = 0, failed = 0;
+    var numUploads, uploaded = 0,
+        failed = 0;
 
     $scope.onFileSelect = function($files) {
         numUploads = $files.length;
@@ -12,46 +14,46 @@ module.exports = function($scope, $upload, $location, $route, uploadedTracks, $q
         uploadedTracks.clear();
 
         for (var i = 0; i < numUploads; i++) {
-
-            var file = $files[i];
-            $scope.upload = $upload.upload({
-                url: 'upload',
-                file: file
-            }).progress(
-                function(evt) {
-                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-
-                }).success(function(data, status, headers, config) {
-                uploaded++;
-
-                console.log("upload finished: ", data);
-
-                uploadedTracks.addTrack(Track.create(data));
-
-                if ($location.path() === "/uploadView") {
-                    $route.reload();
-                } else {
-                    $location.path("/uploadView");
-                }
-            }).error(function(errorMsg, errorCode, headersGetter, data) {
-                failed++;
-
-                console.log("Error uploading file: ", data.file.name);
-                uploadedTracks.addUploadFailedInfo(data.file.name);
-            });
+            upload($scope, $files[i]);
         }
 
         waitForUploads().then(function(data) {
-            console.log('Success: ' + data);
+            console.log("Success: " + data);
             uploadedTracks.serialize();
         }, function(reason) {
-            console.log('Failed: ' + reason);
+            console.log("Failed: " + reason);
         });
 
     };
 
+    function upload($scope, file) {
+
+        $scope.upload = $upload.upload({
+            url: "upload",
+            file: file
+        }).success(function(data) {
+            uploaded++;
+
+            console.log("upload finished: ", data);
+
+            uploadedTracks.addTrack(Track.create(data));
+
+            if ($location.path() === "/uploadView") {
+                $route.reload();
+            } else {
+                $location.path("/uploadView");
+            }
+        }).error(function(errorMsg, errorCode, headersGetter, data) {
+            failed++;
+
+            console.log("Error uploading file: ", data.file.name);
+            uploadedTracks.addUploadFailedInfo(data.file.name);
+        });
+    }
+
     function waitForUploads() {
-        var deferred = $q.defer(), id, finishedUploads;
+        var deferred = $q.defer(),
+            id, finishedUploads;
         try {
             id = setInterval(function() {
                 finishedUploads = uploaded + failed;
